592504641d67b841bbc43cf4e0977c9b9830034e
SatoshiShimada <mylinux1204@gmail.com>
2016-10-21 00:19:36 +0900

    Add setting for auto-install and anything

--- WIP_emacs.el
+++ WIP_emacs.el
@@ -1,4 +1,36 @@
 
+;; add load path
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+;; auto-install
+(when (require 'auto-install nil t)
+  (setq auto-install-directory "~/.emacs.d/lisp/")
+  (auto-install-update-emacswiki-package-name t)
+  (auto-install-compatibility-setup))
+
+;; Anything
+(auto-install-batch "anything")
+(when (require 'anything nil t)
+  (setq
+   anything-idle-delay 0.2
+   anything-input-idle-delay 0.1
+   anything-candidate-number-limit 100
+   anything-quick-update t
+   anything-enable-shortcuts 'alphabet)
+  (when (require 'anything-config nil t)
+    (setq anything-su-or-sudo "sudo"))
+  (require 'anything-match-plugin nil t)
+  (when (and (executable-find "cmigemo")
+             (require 'migemo nil t))
+    (require 'anything-migemo nil t))
+  (when (require 'anything-complete nil t)
+    (anything-lisp-complete-symbol-set-timer 150))
+  (require 'anything-show-complete nil t)
+  (when (require 'auto-install nil t)
+    (require 'anything-auto-install nil t))
+  (when (require 'descbinds-anything nil t)
+    (descbinds-anything-install)))
+
 ;; unvisible menubar
 (menu-bar-mode -1)
 
